3bc0f82311a6800431273f14df384f5c16fbf3a2
caseif <me@caseif.net>
2015-10-29 15:11:04 -0400

    Enforce world check in boundary cstr and contains(). Fixes #10.

--- src/main/java/net/caseif/flint/util/physical/Boundary.java
+++ src/main/java/net/caseif/flint/util/physical/Boundary.java
@@ -48,12 +48,55 @@ public class Boundary {
      */
     public static final Boundary INFINITE = new Boundary(
             new Location3D(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY),
-            new Location3D(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY),
-            true
+            new Location3D(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY)
     );
 
+    private final String world;
     private final Location3D lowerBound;
     private final Location3D upperBound;
+
+    /**
+     * Constructs a new {@link Boundary} from the given locations. These
+     * locations should represent complimentary corners of a cuboid which will
+     * be contained by the new {@link Boundary}.
+     *
+     * @param corner1 The first corner of the {@link Boundary}
+     * @param corner2 The second corner of the {@link Boundary}
+     * @throws IllegalArgumentException If one or both location parameters are
+     *     {@code null}, or if the worlds containing the locations do not
+     *     match and {@code ignoreDifferentWorlds} is {@code false}
+     * @since 1.0
+     */
+    public Boundary(Location3D corner1, Location3D corner2) throws IllegalArgumentException {
+        checkState(corner1 != null, "Boundary corner cannot be null");
+        checkState(corner2 != null, "Boundary corner cannot be null");
+        //noinspection ConstantConditions
+        checkState(!(corner1.getWorld().isPresent() && corner2.getWorld().isPresent()
+                        && !corner1.getWorld().get().equals(corner2.getWorld().get())),
+                "Boundary corners cannot have mismatching worlds");
+
+        if (corner1.getWorld().isPresent()) {
+            world = corner1.getWorld().get();
+        } else if (corner2.getWorld().isPresent()) {
+            world = corner2.getWorld().get();
+        } else {
+            world = null;
+        }
+
+        //noinspection ConstantConditions
+        this.lowerBound = new Location3D(
+                world,
+                Math.min(corner1.getX(), corner2.getX()),
+                Math.min(corner1.getY(), corner2.getY()),
+                Math.min(corner1.getZ(), corner2.getZ())
+        );
+        this.upperBound = new Location3D(
+                world,
+                Math.max(corner1.getX(), corner2.getX()),
+                Math.max(corner1.getY(), corner2.getY()),
+                Math.max(corner1.getZ(), corner2.getZ())
+        );
+    }
 
     /**
      * Constructs a new {@link Boundary} from the given locations. These
@@ -68,43 +111,13 @@ public class Boundary {
      *     {@code null}, or if the worlds containing the locations do not
      *     match and {@code ignoreDifferentWorlds} is {@code false}
      * @since 1.0
+     * @deprecated As of Flint 1.1, the sanity check is performed regardless of
+     *     the boolean flag, rendering it obsolete
      */
+    @Deprecated
     public Boundary(Location3D corner1, Location3D corner2, boolean ignoreDifferentWorlds)
             throws IllegalArgumentException {
-        checkState(corner1 != null, "Boundary corner cannot be null");
-        checkState(corner2 != null, "Boundary corner cannot be null");
-        //noinspection ConstantConditions
-        checkState(ignoreDifferentWorlds
-                        || (!corner1.getWorld().isPresent() && !corner2.getWorld().isPresent())
-                        || corner1.getWorld().get().equals(corner2.getWorld().get()),
-                "Boundary corners cannot have mismatching worlds");
-        //noinspection ConstantConditions
-        this.lowerBound = new Location3D(
-                Math.min(corner1.getX(), corner2.getX()),
-                Math.min(corner1.getY(), corner2.getY()),
-                Math.min(corner1.getZ(), corner2.getZ())
-        );
-        this.upperBound = new Location3D(
-                Math.max(corner1.getX(), corner2.getX()),
-                Math.max(corner1.getY(), corner2.getY()),
-                Math.max(corner1.getZ(), corner2.getZ())
-        );
-    }
-
-    /**
-     * Constructs a new {@link Boundary} from the given locations. These
-     * locations should represent complimentary corners of a cuboid which will
-     * be contained by the new {@link Boundary}.
-     *
-     * @param corner1 The first corner of the {@link Boundary}
-     * @param corner2 The second corner of the {@link Boundary}
-     * @throws IllegalArgumentException If one or both location parameters are
-     *     {@code null}, or if the worlds containing the locations do not
-     *     match
-     * @since 1.0
-     */
-    public Boundary(Location3D corner1, Location3D corner2) throws IllegalArgumentException {
-        this(corner1, corner2, false);
+        this(corner1, corner2);
     }
 
     /**
@@ -131,6 +144,9 @@ public class Boundary {
      * Returns whether this {@link Boundary} contains the given location
      * (inclusively).
      *
+     * <p>This will always return false if both this {@link Boundary} and the
+     * provided location contain a world, but the worlds do not match.</p>
+     *
      * @param location The {@link Location3D} to check against this
      *     {@link Boundary}
      * @return Whether this {@link Boundary} contains the given location
@@ -138,6 +154,11 @@ public class Boundary {
      * @since 1.0
      */
     public boolean contains(Location3D location) {
+        // IntelliJ wants to simplify this method to a single statement. Yeah, in your dreams.
+        //noinspection SimplifiableIfStatement
+        if (world != null && location.getWorld().isPresent() && !world.equals(location.getWorld().get())) {
+            return false;
+        }
         return     location.getX() >= getLowerBound().getX() && location.getX() <= getUpperBound().getX()
                 && location.getY() >= getLowerBound().getY() && location.getY() <= getUpperBound().getY()
                 && location.getZ() >= getLowerBound().getZ() && location.getZ() <= getUpperBound().getZ();
