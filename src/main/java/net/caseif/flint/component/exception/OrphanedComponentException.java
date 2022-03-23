8caae1416c2d66e92c948c06fe59f33a679bf3d5
Max Roncace <me@caseif.net>
2022-03-23 00:26:27 -0400

    Update license headers

--- src/main/java/net/caseif/flint/component/exception/OrphanedComponentException.java
+++ src/main/java/net/caseif/flint/component/exception/OrphanedComponentException.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2016, Max Roncace <me@caseif.net>
+ * Copyright (c) 2015-2022, Max Roncace <me@caseif.net>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
